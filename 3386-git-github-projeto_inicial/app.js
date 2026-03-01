db3c7ffff1d338137ad7f7e6ac4d54ff162feee1
RIAN CAMBUY <rian.cambuy.official@gmail.com>
2026-03-01 16:02:31 -0300

    ALTERAÇÃO NO BACKGROUND DA PAGINA

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,12 +1,11 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
